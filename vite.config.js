f355a750c7fdf3deed147857afb5c33b414b461e
ktsabit <ktsabit@gmail.com>
2024-10-05 17:43:14 +0700

    fix: vite build config

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+// vite.config.js
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    resolve: {
+        alias: {
+            $fonts: resolve('./fonts')
+        }
+    },
+    build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        cockpit: resolve(__dirname, "pages/cockpit.html"),
+        mission: resolve(__dirname, "pages/mission.html"),
+      },
+    },
+  },
+});
