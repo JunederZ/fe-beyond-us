3ed04c38c7e09167a4d97bae1c17cfc408ed813d
JunederZ <arjunadimas000@gmail.com>
2024-10-05 17:00:44 +0700

    vite

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+// const startButton = document.getElementById('button-start');
+
+// startButton.addEventListener('click', () => {
+//     console.log('Start button clicked');
+//     document.getElementById('start-overlay').style.visibility = 'visible';
+//     document.getElementById('start-overlay').style.display = 'flex';
+//     document.getElementById('start-overlay').style.backgroundColor = 'rgba(0, 0, 0, 1)';
+// })
