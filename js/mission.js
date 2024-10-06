2f7409d1cd7485004d60475db3bd180abe360989
risal098 <yesitsme811@gmail.com>
2024-10-06 09:09:45 +0700

    hehe keapus, ni button w balikin

--- js/mission.js
+++ js/mission.js
@@ -1,6 +1,14 @@
 const cockpitButton = document.getElementById('cockpit-button');
 const cockpitButton2 = document.getElementById('cockpit-bottom-button');
 
+cockpitButton.addEventListener('click', () => {
+    console.log('Start button clicked');
+    document.body.style.opacity = 0;
+    setTimeout(() => {
+        window.location.href = '/pages/cockpit';
+    }, 1500);
+})
+
 cockpitButton2.addEventListener('click', () => {
     console.log('Start button clicked');
     document.body.style.opacity = 0;
