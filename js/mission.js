85dfe0ec1a585ad68fcdd16d8a3a1b358db71b56
JunederZ <arjunadimas000@gmail.com>
2024-10-06 08:40:40 +0700

    hover tele

--- js/mission.js
+++ js/mission.js
@@ -1,9 +1,20 @@
 const cockpitButton = document.getElementById('cockpit-button');
+const cockpitButton2 = document.getElementById('cockpit-bottom-button');
+
+cockpitButton2.addEventListener('click', () => {
+    console.log('Start button clicked');
+    document.body.style.opacity = 0;
+    setTimeout(() => {
+        window.location.href = '/pages/cockpit';
+    }, 1500);
+})
+
 
 cockpitButton.addEventListener('click', () => {
     console.log('Start button clicked');
     document.body.style.opacity = 0;
     setTimeout(() => {
         window.location.href = '/pages/cockpit';
-    }, 2000);
+    }, 1500);
 })
+
